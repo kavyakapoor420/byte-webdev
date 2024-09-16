a89b68875c69cbf5fa00d922a1f5705899b8ce6e
sakshamg567 <saksham060306@gmail.com>
2024-09-16 22:29:12 +0530

    new

---
+++ api/server.js
@@ -0,0 +1,185 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors'); // Enable Cross-Origin Resource Sharing
+const passport = require('passport');
+const session = require('express-session');
+const GithubStrategy = require('passport-github2').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const axios = require('axios');
+const { Octokit } = require('@octokit/rest');
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
+
+// Configure Github Credentials
+const GITHUB_CLIENT_ID = 'Ov23liKUHoo3Cxk8DzDX';
+const GITHUB_CLIENT_SECRET = '344d0d14ab8e68d3b234d7127baa13fd10ac5d04';
+
+// Configure Google Credentials
+const GOOGLE_CLIENT_ID = '130054163106-1aicss44megmnn6irggit876lrme1h4f.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+const CHANNEL_ID = 'UCgIzTPYitha6idOdrr7M8sQ';
+
+// set up passport.js
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: 'http://localhost:8000/auth/google/callback',
+    scope: ['https://www.googleapis.com/auth/youtube.readonly']
+}, (accessToken, refreshToken, profile, done) => {
+    profile.accessToken = accessToken;
+    return done(null, profile);
+}));
+
+
+passport.use(new GithubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: 'http://localhost:8000/auth/github/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+        profile.accessToken = accessToken;
+        return done(null, profile);
+    }
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } 
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/login_screen.html'));
+});
+
+// Routes for Google Login
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['openid', 'profile', 'email', 'https://www.googleapis.com/auth/youtube.readonly'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }), async(req, res) => 
+    {
+        const { accessToken } = req.user;
+        req.session.googleaccessToken = accessToken;
+        try {
+            const response = await axios.get(`https://www.googleapis.com/youtube/v3/subscriptions?part=snippet&forChannelId=${CHANNEL_ID}&mine=true`, {
+                headers: { Authorization: `Bearer ${accessToken}` }
+            });
+            const isSubscribed = response.data.items.length > 0;
+
+            req.session.isSubscribed = isSubscribed;
+
+            if(isSubscribed) {
+                res.redirect('/login/success');
+            }else {
+                res.redirect('/youtube/verification/failed');
+            }
+        }
+        catch(error) {
+                res.send('Error checking subscription');
+            }
+        }
+);
+
+// Github Routes
+
+app.get('/auth/github', passport.authenticate('github', { scope: ['user:email'] }));
+
+app.get('/auth/github/callback', 
+    passport.authenticate('github', { failureRedirect: '/' }),
+    async (req, res) => {
+        const { accessToken } = req.user;
+        const octokit = new Octokit({
+            auth: accessToken
+        });
+
+        try {
+            const response = await octokit.request('GET /user/following/bytemait', {
+                headers: {
+                    'X-GitHub-Api-Version': '2022-11-28'
+                }
+            });
+            if (response.status === 204) {
+                res.redirect('/login/success');
+            } else {
+                res.redirect('/github/verification/failed');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+
+            res.redirect('/github/verification/failed');
+        }
+    }
+);
+
+
+async function ensureSubscribed(req, res, next) {
+    if (req.isAuthenticated() && req.session.googleaccessToken) {
+        const accessToken = req.session.googleaccessToken;
+        try {
+            const response = await axios.get(`https://www.googleapis.com/youtube/v3/subscriptions?part=snippet&forChannelId=${CHANNEL_ID}&mine=true`, {
+                headers: { Authorization: `Bearer ${accessToken}` }
+            });
+            const isSubscribed = response.data.items.length > 0;
+
+            req.session.isSubscribed = isSubscribed;
+
+            if(isSubscribed) {
+                return next(); 
+            }else {
+                res.redirect('/youtube/verification/failed');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            res.redirect('/youtube/verification/failed');
+        }
+    }   else {
+        return res.redirect('/');
+    }
+}
+
+app.get('/youtube/verification/failed', (req, res) => {
+    if(req.isAuthenticated()) {
+        res.sendFile(path.join(__dirname, 'public/youtube_verification_failed.html'));
+    }else {
+        res.redirect('/');
+    }
+});
+
+app.get('/github/verification/failed', (req, res) => {
+    if(req.isAuthenticated()) {
+        res.sendFile(path.join(__dirname, 'public/github_verification_fail.html'));
+    }else {
+        res.redirect('/');
+    }
+});
+
+app.get('/login/success', ensureSubscribed, (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/success.html'));
+});
+
+app.get('*', (req, res) => {
+    res.redirect('/'); // Redirect to the homepage
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+
